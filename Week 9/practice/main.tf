234abe85e138c77cbf996cd0b0881e09be2ac583
iampsrv <iampsrv@gmail.com>
2025-07-13 19:53:37 +0530

    Week 9 res

--- Week 9/practice/main.tf
+++ Week 9/practice/main.tf
@@ -81,3 +81,73 @@ module "batch29" {
   name_sg = "mysgmod29"
   # insert the 4 required variables here
 }
+#########################
+resource "aws_instance" "example" {
+  count         = 3
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
+#########################
+variable "servers" {
+  default = {
+    server1 = "t2.micro"
+    server2 = "t3.micro"
+  }
+}
+
+resource "aws_instance" "example" {
+  for_each      = var.servers
+  ami           = "ami-12345678"
+  instance_type = each.value
+  tags = {
+    Name = each.key
+  }
+}
+
+
+#########################
+# Define a map of instances with their specific details
+variable "ec2_configs" {
+  default = {
+    instance1 = {
+      ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu
+      instance_type = "t2.micro"
+      name          = "WebServer"
+    }
+    instance2 = {
+      ami           = "ami-07d02ee1eeb0c996c"  # Amazon Linux
+      instance_type = "t3.micro"
+      name          = "AppServer"
+    }
+    instance3 = {
+      ami           = "ami-06ca3ca175f37dd66"  # Debian
+      instance_type = "t3a.micro"
+      name          = "DBServer"
+    }
+  }
+}
+
+resource "aws_instance" "custom_instances" {
+  for_each = var.ec2_configs
+
+  ami           = each.value.ami
+  instance_type = each.value.instance_type
+
+  tags = {
+    Name = each.value.name
+  }
+}
+################
+
+variable "amis"           { default = ["ami-0c55b159cbfafe1f0", "ami-07d02ee1eeb0c996c", "ami-06ca3ca175f37dd66"] }
+variable "instance_types" { default = ["t2.micro", "t3.micro", "t3a.micro"] }
+variable "names"          { default = ["WebServer", "AppServer", "DBServer"] }
+
+resource "aws_instance" "demo" {
+  count = length(var.amis)          # == 3
+
+  ami           = var.amis[count.index]
+  instance_type = var.instance_types[count.index]
+
+  tags = { Name = var.names[count.index] }
+}
